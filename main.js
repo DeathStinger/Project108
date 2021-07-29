4558638f0c5affd665d460f520ac8b30f93e1931
DeathStinger <87364785+DeathStinger@users.noreply.github.com>
2021-07-29 13:08:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+prediction="";
+Webcam.set({
+width:350,
+height:300,
+image_format: "jpeg",
+jpeg_quality: 100
+});
+camera =document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+  Webcam.snap(function (data_uri){
+      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+  });
+};
+console.log('ml5 version:',ml5.version);
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/FwOXlfquK/model.json",ModelLoaded);
+function ModelLoaded() {
+   console.log("Model Loaded"); 
+}
+function speak() {
+ var synth= window.speechSynthesis;
+ speak_data="The prediction is"+ prediction; 
+var utterThis = new SpeechSynthesisUtterance(speak_data);
+ utterThis.rate = 0.5;
+ synth.speak(utterThis);
+}
+function check() {
+ img = document.getElementById('captured_image');
+ classifier.classify(img,gotResult);
+}
+ function gotResult(error,results) {
+   if (error) {
+     console.log(error);
+   } else {
+    console.log(results); 
+    document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    prediction=results[0].label;  
+    speak();
+    if (results[0].label == "Rock & Roll " ) {
+     document.getElementById("update_gesture").innerHTML = "&#129304";
+    }
+    if (results[0].label == "Thumbs up" ) {
+      document.getElementById("update_gesture").innerHTML = "&#128077";
+    }
+
+      if (results[0].label == "Thumbs dowm" ) {
+        document.getElementById("update_gesture").innerHTML = "&#128078";
+     }
+     if (results[0].label == "Amazing" ) {
+      document.getElementById("update_gesture").innerHTML = "&#9996";
+   }
+   if (results[0].label == "Victory" ) {
+    document.getElementById("update_gesture").innerHTML = "&#128076";
+ }
+ 
+    }
+  }
+
+
